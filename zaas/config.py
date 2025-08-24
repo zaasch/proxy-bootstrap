83a2d57f2fa8b68adb6bb34948fcd9bd0e316018
Alexandre Georges <alexandre@mageo.ch>
2025-08-24 08:37:09 +0200

    Refactor Config class to specify types for configuration fields

--- zaas/config.py
+++ zaas/config.py
@@ -4,10 +4,10 @@ from pydantic import Field
 
 class Config(BaseSettings):
 
-    LOGFILE = Field("/var/log/zaas-bootstrap.log", description="Path to the log file")
-    CONFIG_DIR = Field("/etc/zaas", description="Path to the config directory")
-    CONFIG_FILE = Field("zaas.json", description="Path to the config file")
-    UUID_FILE = Field("uuid", description="Path to the UUID file")
+    LOGFILE: str = Field("/var/log/zaas-bootstrap.log", description="Path to the log file")
+    CONFIG_DIR: str = Field("/etc/zaas", description="Path to the config directory")
+    CONFIG_FILE: str = Field("zaas.json", description="Path to the config file")
+    UUID_FILE: str = Field("uuid", description="Path to the UUID file")
 
 
-config = Config()
+config = Config()  # type: ignore
