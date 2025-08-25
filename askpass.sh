f2038820c89f15920ebe5d8c54e9d8fe0189cf62
Alexandre Georges <alexandre@mageo.ch>
2025-08-25 11:07:49 +0200

    Refactor bootstrap script to remove sudo and improve root check; update askpass script to retrieve token correctly

--- askpass.sh
+++ askpass.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 case "$1" in
   *Username*) echo "x-access-token" ;;
-  *Password*) cat /etc/zaas/.github_token ;;
+  *Password*) cat /etc/zaas/.github_token | jq -r .token ;;
   *) echo "" ;;
 esac
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+# Make sure we are root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
 # Install required packages
 echo -n "Installing required packages..."
-sudo apt install -y python3-venv > /dev/null 2>&1
+apt install -y python3-venv > /dev/null 2>&1
 echo " done."
 
 # Clone full repository
 echo -n "Cloning repository..."
-sudo rm -rf proxy-bootstrap > /dev/null 2>&1
+rm -rf proxy-bootstrap > /dev/null 2>&1
 git clone https://github.com/zaasch/proxy-bootstrap.git proxy-bootstrap > /dev/null 2>&1
 echo " done."
 
@@ -24,10 +30,18 @@ pip install -r requirements.txt > /dev/null 2>&1
 echo " done."
 
 # Run the bootstrap process
-sudo ./.venv/bin/python3 bootstrap.py
+./.venv/bin/python3 bootstrap.py
 
 # Run the register process
-sudo ./.venv/bin/python3 register.py
+./.venv/bin/python3 register.py
+
+# Create the zaas user if it does not exist
+if ! id -u zaas > /dev/null 2>&1; then
+  useradd -m -U zaas
+fi
 
 # Run ansible-pull for the first time
-sudo GIT_ASKPASS=$(pwd)/askpass.sh GIT_TERMINAL_PROMPT=0 ./.venv/bin/ansible-pull -U https://github.com/zaasch/managed-proxy.git -i localhost
+chmod +x askpass.sh
+GIT_ASKPASS=$(pwd)/askpass.sh \
+GIT_TERMINAL_PROMPT=0 \
+./.venv/bin/ansible-pull -U https://github.com/zaasch/managed-proxy.git -i localhost
